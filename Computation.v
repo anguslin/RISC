e28e91f52db8e1897d1275e1c06583f0efeeba4b
anguslin <angus.a.lin@gmail.com>
2016-05-20 14:48:36 -0700

    Update Computation.v

--- Computation.v
+++ Computation.v
@@ -15,7 +15,7 @@ module Computation(shift, ALUop, bsel, loadc, loads);
 			4’bx0xx: Bin = Bshift;	                      	  	//if bsel is 0 set Bin = to shifted B value
 			4’bx1xx: Bin = {{11{1’b0}},datapath_in[4:0]};		//if bsel is 1 set Bin = to 11bits 0s + first 5 bits of datapath_in
 			4’bxx1x: CToUpdate = ALUComputedValue			//if loadc is 1 load computed ALU value into CToUpdate (wait for clock)
-			4’bxxx1: statusToUpdate = statusComputed		//if loadc is 1 load computed status value into statusToUpdate (wait for clock)
+			4’bxxx1: statusToUpdate = statusComputed			//if loadc is 1 load computed status value into statusToUpdate (wait for clock)
 			
 			default: {Ain, Bin, CToUpdate, statusToUpdate} = {49{1’bx}}      //set everything to x for default
 		endcase
@@ -37,8 +37,8 @@ module Computation(shift, ALUop, bsel, loadc, loads);
 	always @(*) begin
 		case(shift)
 			2’b00: Bshift = B;				//if operation is 00 output is B
-			2’b01: Bshift = {B[14:0], 1’b0};		//if operation is 01 output is B shifted left one and right bit becomes 0 
-			2’b10: Bshift = {1’b0, B[15:1]};		//if operation is 10 output is B shifted right one and left bit becomes 0 
+			2’b01: Bshift = {B[14:0], 1’b0};			//if operation is 01 output is B shifted left one and right bit becomes 0 
+			2’b10: Bshift = {1’b0, B[15:1]};			//if operation is 10 output is B shifted right one and left bit becomes 0 
 			2’b11: Bshift = {B[15], B[15:1]};		//if operation is 11 output is B shifted right one and left bit becomes b[15]
 		endcase
 	end
