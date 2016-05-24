b36860307eb312c0d66231dc6975393ca60ccfca
anguslin <angus.a.lin@gmail.com>
2016-05-24 16:09:54 -0700

    Create ALU.v

---
+++ ALU.v
@@ -0,0 +1,17 @@
+//ALU operations
+module ALU(ALUop, Ain, Bin, ALUComputedValue);
+
+	input [1:0] ALUop;
+	input [15:0] Ain, Bin;
+	output [15:0] ALUComputedValue;
+
+	always @(*) begin
+		case(ALUop)
+			2'b00: ALUComputedValue= Ain + Bin;		//if operation= 00 output= Ain + Bin
+			2'b01: ALUComputedValue= Ain - Bin;		//if operation= 01 output= Ain - Bin
+			2'b10: ALUComputedValue= Ain & Bin;		//if operation= 10 output= Ain AND Bin
+			2'b11: ALUComputedValue= ~Bin;			//if operation= 11 output= not Bin
+			default: ALUComputedValue= {`WIDTH{1'bx}};	//default all x
+		endcase
+	end
+endmodule
