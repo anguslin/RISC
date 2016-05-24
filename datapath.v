b8b667aab0b1128b0c9d0864c309ac6a0d907942
anguslin <angus.a.lin@gmail.com>
2016-05-24 13:49:39 -0700

    Update datapath.v

--- datapath.v
+++ datapath.v
@@ -1,16 +1,19 @@
-module datapath(clk, readnum, vsel, loada, loadb, shift, axel, bel, ALUop, loadc, loads, writenum, write, datapath_in, status, datapath_out)
-input clk, vsel, loada, loadb, write, asel, bsel, loadc, loads;
-input [2:0] readnum, writenum;
-input [15:0] datapath_in;
-input [1:0] shift, ALUop
-output [15:0] datapath_out;
-wire [15:0] reg0, reg1, reg2, reg3, reg4 ,reg5 ,reg6 ,reg7, Ain, Bin, BShift, ALUComputedValue, C, A, B, statusComputed, status;
-reg [15:0] AToUpdate, BToUpdate, CToUpdate, statusToUpdate, data_out, data_in;
 
+module datapath(clk, readnum, vsel, loada, loadb, shift, asel, bsel, ALUop, loadc, loads, writenum, write, datapath_in, status, datapath_out);
 
-//module Register(clk, vsel, loada, loadb, write, readnum)
+	//constants to define
+	`define WIDTH 16
+	`define STATUSWIDTH 1
 
-
+	input clk, loada, loadb, write, vsel, asel, bsel, loadc, loads;
+	input [2:0] readnum, writenum;
+	input [1:0] shift, ALUop;
+	input [15:0] datapath_in;
+	output [15:0] datapath_out;
+	output status
+	
+	wire [15:0] A, B, C, reg0, reg1, reg2, reg3, reg4 ,reg5 ,reg6 ,reg7;
+	
 
 Register instantiateReg(
   .clk(clk)
