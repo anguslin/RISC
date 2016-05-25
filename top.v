845253c8b204b024d19576c2074158421589993c
anguslin <angus.a.lin@gmail.com>
2016-05-25 16:56:18 -0700

    modified HEX display module

--- top.v
+++ top.v
@@ -77,17 +77,19 @@ module top(KEY, SW, CLOCK_50, LEDR, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5);
                         .datapath_out(datapath_out)
                      );
         
-          // fill in sseg to display 4-bits in hexidecimal 0,1,2...9,A,B,C,D,E,F
-          sseg H0(datapath_out[3:0],   HEX0);   
-          sseg H1(datapath_out[7:4],   HEX1);
-          sseg H2(datapath_out[11:8],  HEX2);
-          sseg H3(datapath_out[15:12], HEX3);
+          // assigned HEX display to datapath_out values
+          HEXDisplay HEX0(datapath_out[3:0], HEX0);   
+          HEXDisplay HEX1(datapath_out[7:4], HEX1);
+          HEXDisplay HEX2(datapath_out[11:8], HEX2);
+          HEXDisplay HEX3(datapath_out[15:12], HEX3);
+          
           assign HEX4 = 7'b1111111;  // disabled
           assign HEX5 = 7'b1111111;  // disabled
-        endmodule
+endmodule
         
-        module input_iface(clk, SW, datapath_in, write, vsel, loada, loadb, asel, bsel, 
-                           loadc, loads, readnum, writenum, shift, ALUop, LEDR);
+//Two possibilities depending on value of SW[9]
+module input_iface(clk, SW, datapath_in, write, vsel, loada, loadb, asel, bsel, loadc, loads, readnum, writenum, shift, ALUop, LEDR);
+
           input clk;
           input [9:0] SW;
           output [15:0] datapath_in;
@@ -103,42 +105,22 @@ module top(KEY, SW, CLOCK_50, LEDR, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5);
           vDFF #(16) DATA(clk,datapath_in_next,datapath_in);
         
           // When SW[9] is set to 0, SW[8:0] changes the control inputs 
-          //
           wire [8:0] ctrl_sw;
           wire [8:0] ctrl_sw_next = sel_sw ? ctrl_sw : SW[8:0];
           vDFF #(9) CTRL(clk,ctrl_sw_next,ctrl_sw);
         
           assign {readnum,vsel,loada,loadb,shift,asel,bsel,ALUop,loadc,loads,writenum,write}={
-            // register operand fetch stage
-            //     readnum       vsel        loada       loadb
-                   ctrl_sw[3:1], ctrl_sw[4], ctrl_sw[5], ctrl_sw[6], 
-            // computation stage (sometimes called "execute")
-            //     shift         asel        bse         ALUop         loadc       loads
-                   ctrl_sw[2:1], ctrl_sw[3], ctrl_sw[4], ctrl_sw[6:5], ctrl_sw[7], ctrl_sw[8],  
-            // set when "writing back" to register file
-            //   writenum        write
-                   ctrl_sw[3:1], ctrl_sw[0]    
-          };
-        
+                 //readnum       vsel        loada       loadb       shift         asel        bse         ALUop         loadc       loads       writenum      write
+                   ctrl_sw[3:1], ctrl_sw[4], ctrl_sw[5], ctrl_sw[6], ctrl_sw[2:1], ctrl_sw[3], ctrl_sw[4], ctrl_sw[6:5], ctrl_sw[7], ctrl_sw[8], ctrl_sw[3:1], ctrl_sw[0]};
           // LEDR[7:0] shows other bits
           assign LEDR = sel_sw ? ctrl_sw : {1'b0, datapath_in[7:0]};  
-        endmodule         
+endmodule         
         
-        // The sseg module below can be used to display the value of datpath_out on
-        // the hex LEDS the input is a 4-bit value representing numbers between 0 and
-        // 15 the output is a 7-bit value that will print a hexadecimal digit.  You
-        // may want to look at the code in Figure 7.20 and 7.21 in Dally but note this
-        // code will not work with the DE1-SoC because the order of segments used in
-        // the book is not the same as on the DE1-SoC (see comments below).
-        
-        module sseg(in,segs);
-          input [3:0] in;
-          output [6:0] segs;
-        
+//module to display the value of datpath_out DE1-SoC
+
+ // One bit per segment on the DE1-SoC a HEX segment is illuminated depending on the input from datapath_out
+          //The HEX display bits are as follows:
           
-          // One bit per segment. On the DE1-SoC a HEX segment is illuminated when
-          // the input bit is 0. Bits 6543210 correspond to:
-          //
           //    0000
           //   5    1
           //   5    1
@@ -146,25 +128,28 @@ module top(KEY, SW, CLOCK_50, LEDR, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5);
           //   4    2
           //   4    2
           //    3333
-          //
-          // Decimal value | Hexadecimal symbol to render on (one) HEX display
-          //             0 | 0
-          //             1 | 1
-          //             2 | 2
-          //             3 | 3
-          //             4 | 4
-          //             5 | 5
-          //             6 | 6
-          //             7 | 7
-          //             8 | 8
-          //             9 | 9
-          //            10 | A
-          //            11 | b
-          //            12 | C
-          //            13 | d
-          //            14 | E
-          //            15 | F
-        
-          assign segs = 7'b0001110;  // this will output "F" 
-
+          
+module HEXDisplay(inValue, display);
+        input [3:0] inValue;
+        output [6:0] display;
+        always @(*) begin
+                case(inValue)
+                        4'b0000: display = 7'b1000000 //0
+                        4'b0001: display = 7'b1111001 //1
+                        4'b0010: display = 7'b0100100 //2
+                        4'b0011: display = 7'b0110000 //3
+                        4'b0100: display = 7'b0011001 //4
+                        4'b0101: display = 7'b0010010 //5
+                        4'b0110: display = 7'b0000010 //6
+                        4'b0111: display = 7'b1111000 //7
+                        4'b1000: display = 7'b0000000 //8
+                        4'b1001: display = 7'b0011000 //9
+                        4'b1010: display = 7'b0001000 //10 = A
+                        4'b1011: display = 7'b0000011 //11 = b
+                        4'b1100: display = 7'b1000110 //12 = C
+                        4'b1101: display = 7'b0100001 //13 = d
+                        4'b1110: display = 7'b0000110 //14 = E
+                        4'b1111: display = 7'b0001110 //15 = F
+                endcase
+        end
 endmodule
