137762499e8b408ed6b1c03967baf5d670870473
anguslin <angus.a.lin@gmail.com>
2016-05-24 17:13:02 -0700

    Update Register.v

--- Register.v
+++ Register.v
@@ -1,5 +1,6 @@
 module Register(clk, loada, loadb, write, readnum, reg0, reg1, reg2, reg3, reg4 ,reg5 ,reg6 ,reg7, A, B);
   
+  	parameter width= 1;
 	input clk, loada, loadb, write;
 	input [2:0] readnum;
 	input [15:0] reg0, reg1, reg2, reg3, reg4 ,reg5 ,reg6 ,reg7;
@@ -16,12 +17,11 @@ module Register(clk, loada, loadb, write, readnum, reg0, reg1, reg2, reg3, reg4 
 			3'b101: data_out= write? data_out: reg5; //if write= 0 and readnum= 101 data_out = reg5
 			3'b110: data_out= write? data_out: reg6; //if write= 0 and readnum= 110 data_out = reg6
 			3'b111: data_out= write? data_out: reg7; //if write= 0 and readnum= 111 data_out = reg7
-			default: data_out= {`WIDTH{1'bx}};	 //set everything to x for default
+			default: data_out= {width{1'bx}};	 //set everything to x for default
 		endcase
 	end
 
 	//Clock Updates for A and B depending on clock and loada and loadb
-	DFlipFlopAllow #(`WIDTH) loadaData(clk, loada, data_out, A);
-	DFlipFlopAllow #(`WIDTH) loadbData(clk, loadb, data_out, B);
-	
+	DFlipFlopAllow #(width) loadaData(clk, loada, data_out, A);
+	DFlipFlopAllow #(width) loadbData(clk, loadb, data_out, B);
 endmodule
