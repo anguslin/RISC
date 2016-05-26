5e4129f281348979058fb4e0a3dfd17ece44bcd8
anguslin <angus.a.lin@gmail.com>
2016-05-26 14:10:54 -0700

    fixed spelling error for variable

--- DFlipFlopAllow.v
+++ DFlipFlopAllow.v
@@ -1,12 +1,12 @@
 //D Flip Flop with enabling property
 
 module DFlipFlopAllow(clk, allow, in, out) ;
-  parameter n = 1;  // width
+  parameter width = 1;  // width
   input clk, allow ;
-  input [n-1:0] in ;
-  output [n-1:0] out ;
-  reg [n-1:0] out ;
-  wire toBeUpdate [n-1:0];
+  input [width-1:0] in ;
+  output [width-1:0] out ;
+  reg [width-1:0] out ;
+  wire [width-1:0] toBeUpdated;
 
 assign toBeUpdated = allow? in : out; //only update if allowed
 
