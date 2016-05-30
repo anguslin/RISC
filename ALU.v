d51aa257c74c9f33b91d4709dfddc748a569238a
anguslin <angus.a.lin@gmail.com>
2016-05-30 16:44:18 -0700

    fixed discrepancy between module instantiations

--- ALU.v
+++ ALU.v
@@ -1,28 +1,27 @@
 //ALU operations
 module ALU(ALUop, Ain, Bin, ALUComputedValue, overflow);
 
-	`define ADD 00
-	`define SUB 01
-	`define ANDVAL 10
-	`define NOTB 11
+	`define ADD 2'b00
+	`define SUB 2'b01
+	`define ANDVAL 2'b10
+	`define NOTB 2'b11
 
 	parameter width= 1;
-	input sub, overflow;
 	input [1:0] ALUop;
 	input [15:0] Ain, Bin;
 	output [15:0] ALUComputedValue;
 	output overflow;
 	reg [15:0] ALUComputedValue;
 	wire [15:0] computedValueTemp;
-	wire addSubVals, andVals, notBVal, sub;
+	reg addSubVals, andVals, notBVal, sub;
        
 	always @(*) begin
 		case(ALUop)
-			`ADD: {addSubVals, andVals, notBVal, sub}= {4'b1000}	
-			`SUB: {addSubVals, andVals, notBVal, sub}= {4'b1001}		
-			`ANDVAL: {addSubVals, andVals, notBVal, sub}= {4'b0100}		
-			`NOTB: {addSubVals, andVals, notBVal, sub}= {4'b0010}			
-			default: {addSubVals, andVals, notBVal, sub}= {4'bxxxx}	//default all x
+			`ADD: {addSubVals, andVals, notBVal, sub}= {4'b1000};	
+			`SUB: {addSubVals, andVals, notBVal, sub}= {4'b1001};		
+			`ANDVAL: {addSubVals, andVals, notBVal, sub}= {4'b0100};		
+			`NOTB: {addSubVals, andVals, notBVal, sub}= {4'b0010};			
+			default: {addSubVals, andVals, notBVal, sub}= {4'bxxxx};	//default all x
 		endcase
 	end
 	
