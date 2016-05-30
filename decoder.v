8b9929adb6d285b02d9d6358b3090714be2df9d0
anguslin <angus.a.lin@gmail.com>
2016-05-30 15:30:18 -0700

    Assigned more values for decoding

--- decoder.v
+++ decoder.v
@@ -1,10 +1,19 @@
+//decoder module
 module decoder(instruction);
-input[15:0] instruction
-output [2:0] opcode, Rn, Rd, Rm;
-output [1:0] ALUop, op, shift
-output [15:0] sximm5, sximm8;
-
-assign opcode = instruction[15:13];
-assign op = instruction[12:11];
-assign ALUop = instruction[12:11];
-assign sximm5 = {10'b
+        //inputs and outputs
+        input[15:0] instruction
+        output [2:0] opcode, Rn, Rd, Rm;
+        output [1:0] ALUop, op, shift
+        output [15:0] sximm5, sximm8;
+        
+        //assignments based off of instruction
+        assign opcode = instruction[15:13];
+        assign op = instruction[12:11];
+        assign ALUop = instruction[12:11];
+        assign sximm5 = {10{instruction[4]},instruction[4:0]};
+        assign sximm8 = {7{instruction[7]},instruction[7:0]};
+        assign shift = instruction[4:3];
+        assign Rn = instruction[10:8];
+        assign Rd = instruction[7:5];
+        assign Rm = instruction[2:0];
+endmodule
