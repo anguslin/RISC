f0126009bea5d221c4e56195e4dd32942cc36de9
anguslin <angus.a.lin@gmail.com>
2016-05-30 09:38:14 -0700

    rising clock update for output

--- counter.v
+++ counter.v
@@ -1,14 +1,17 @@
 //module for program counter
 
-module counter(reset, loadpc, out);
+module counter(reset, loadpc, msel, out, C);
         
         `define COUNTWIDTH 8;
-        input reset, loadpc;
+        input reset, loadpc, msel;
+        input [15:0] C;
         output [7:0] out;
-        wire [7:0] loadValue, outToBeUpdated;
+        wire [7:0] loadValue, countOut, countOutToBeUpdated;
         
         assign loadValue = loadpc? out + 1'b1 : out;
+        assign out = msel? C[7:0] : countOut;
         assign outToBeUpdated= reset? 8'b00000000 : loadValue;
-        DFlipFlop #(`COUNTWIDTH) loadOut(clk, outToBeUpdated, out);
+        //update on rising clock
+        DFlipFlop #(`COUNTWIDTH) loadOut(clk, countOutToBeUpdated, countOut); 
 
 endmodule
