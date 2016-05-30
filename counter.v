531c30bc0febc332997999c71c1d6e1aabfaede7
anguslin <angus.a.lin@gmail.com>
2016-05-30 16:53:02 -0700

    fixed assign statements

--- counter.v
+++ counter.v
@@ -1,17 +1,19 @@
-//module for program counter
-
-module counter(reset, loadpc, msel, out, C);
+//decoder module
+module decoder(instruction);
+        //inputs and outputs
+        input[15:0] instruction;
+        output [2:0] opcode, Rn, Rd, Rm;
+        output [1:0] ALUop, op, shift;
+        output [15:0] sximm5, sximm8;
         
-        `define COUNTWIDTH 8
-        input reset, loadpc, msel;
-        input [15:0] C;
-        output [7:0] out;
-        wire [7:0] loadValue, countOut, countOutToBeUpdated;
-        
-        assign loadValue = loadpc? out + 1'b1 : out;
-        assign out = msel? C[7:0] : countOut;
-        assign outToBeUpdated= reset? 8'b00000000 : loadValue;
-        //update on rising clock
-        DFlipFlop #(`COUNTWIDTH) loadCountOut(clk, countOutToBeUpdated, countOut); 
-
+        //assignments based off of instruction
+        assign opcode = instruction[15:13];
+        assign op = instruction[12:11];
+        assign ALUop = instruction[12:11];
+        assign sximm5 = instruction[4]? {10'b1111111111,instruction[4:0]} : {10'b0000000000, instruction[4:0]};
+	assign sximm8 = instruction[7]? {7'b1111111,instruction[7:0]} : {7'b0000000, instruction[7:0]};
+        assign shift = instruction[4:3];
+        assign Rn = instruction[10:8];
+        assign Rd = instruction[7:5];
+        assign Rm = instruction[2:0];
 endmodule
