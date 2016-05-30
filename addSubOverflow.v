bf887551cb4bd2fa000cc32da59e63dc37427daa
anguslin <angus.a.lin@gmail.com>
2016-05-30 11:37:52 -0700

    added more operations to perform

--- addSubOverflow.v
+++ addSubOverflow.v
@@ -1,19 +1,35 @@
-module addSubOverflow (Ain, Bin, status, overflow, sub, computedValue);
+module operation (Ain, Bin, status, overflow, addSubVals, andVals, notBVal, sub, computedValue);
 
         parameter width= 1;
         input [width-1:0] Ain, Bin;
-        input sub, overflow;
+        input overflow, addSubVals, andVals, notBVal, sub;
         output [2:0] status;
         output [width-1:0] computedValue;
         wire sign, lastNonSign;
         
-        assign overflow = sign ^ lastNonSign; //XOR gate; if not the same => overflow
+        always@(*) begin
+                if(addSubVals) begin
+                        assign overflow = sign ^ lastNonSign; //XOR gate; if not the same => overflow
+                        
+                        //Arithmetic on Non Sign Bits
+                        //if subtract, then convert to Two's Complement and then add
+                        assign {lastNonSign, computedValue[width-2:0]}= {Ain[width-2:0] + Bin[width-2:0] ^ {width-1{sub}} + sub;
+                        
+                        //Arithmetic on Sign Bits
+                        assign {sign, computedValue[width-1]}= {Ain[width-1] + (Bin[width-1] ^ sub) + lastNonSign;
+                end
+        end
         
-        //Arithmetic on Non Sign Bits
-        //if subtract, then convert to Two's Complement and then add
-        assign {lastNonSign, computedValue[width-2:0]}= {Ain[width-2:0] + Bin[width-2:0] ^ {width-1{sub}} + sub;
+        always@(*) begin
+                if(andVals) begin
+                        computedValue = Ain & Bin;
+                end
+        end
         
-        //Arithmetic on Sign Bits
-        assign {sign, computedValue[width-1]}= {Ain[width-1] + (Bin[width-1] ^ sub) + lastNonSign;
+        always@(*) begin
+                if(notBVal) begin
+                        computedValue = ~Bin;
+                end
+        end
 
 endmodule
