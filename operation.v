1b361f82724b6dcb9432b7b6b71518553a414c2a
anguslin <angus.a.lin@gmail.com>
2016-05-30 12:33:33 -0700

    modified into case statement

--- operation.v
+++ operation.v
@@ -8,27 +8,19 @@ module operation (Ain, Bin, overflow, addSubVals, andVals, notBVal, sub, compute
         
         always@(*) begin
         //subtraction or add
-                if(addSubVals) begin
-                        assign overflow = sign ^ lastNonSign; //XOR gate; if not the same => overflow
-                        //Arithmetic on Non Sign Bits
-                        //if subtract, then convert to Two's Complement and then add
-                        assign {lastNonSign, computedValue[width-2:0]}= {Ain[width-2:0] + Bin[width-2:0] ^ {width-1{sub}} + sub;
-                        //Arithmetic on Sign Bits
-                        assign {sign, computedValue[width-1]}= {Ain[width-1] + (Bin[width-1] ^ sub) + lastNonSign;
-                end
-        end
-        
-        always@(*) begin
-        //if and A and B
-                if(andVals) begin
-                        {computedValue, overflow} = {Ain & Bin, 1'b0};
-                end
-        end
-        
-        always@(*) begin
-        //if not B 
-                if(notBVal) begin
-                        {computedValue, overflow} = {~Bin, 1'b0};
-                end
+                case({addSubVals, andVals, notBVal})
+                        3'b100:
+                                overflow = sign ^ lastNonSign; //XOR gate; if not the same => overflow
+                                //Arithmetic on Non Sign Bits
+                                //if subtract, then convert to Two's Complement and then add
+                                {lastNonSign, computedValue[width-2:0]}= {Ain[width-2:0] + Bin[width-2:0] ^ {width-1{sub}} + sub;
+                                //Arithmetic on Sign Bits
+                                {sign, computedValue[width-1]}= {Ain[width-1] + (Bin[width-1] ^ sub) + lastNonSign;
+                        3'b010: //If and A with B
+                                {computedValue, overflow} = {Ain & Bin, 1'b0};
+                        3'b001: //If not B
+                                {computedValue, overflow} = {~Bin, 1'b0};
+                        default: 
+                                {computedValue, overflow} = {width+1{1'bx}};
         end
 endmodule
