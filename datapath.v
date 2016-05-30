17c60d2e11b20c62fa17a7815686149272e9d18f
anguslin <angus.a.lin@gmail.com>
2016-05-30 16:14:57 -0700

    modified names

--- datapath.v
+++ datapath.v
@@ -3,14 +3,14 @@ module datapath(clk, readnum, vsel, loada, loadb, shift, asel, bsel, ALUop, load
 
 	//constants to define
 	`define WIDTH 16
-	`define STATUSWIDTH 1
+	`define STATUSWIDTH 3
 
 	input clk, loada, loadb, write, vsel, asel, bsel, loadc, loads;
 	input [2:0] readnum, writenum;
 	input [1:0] shift, ALUop;
 	input [15:0] datapath_in;
 	output [15:0] datapath_out;
-	output status;
+	output [2:0] status;
 	wire [15:0] A, B, C, reg0, reg1, reg2, reg3, reg4, reg5, reg6, reg7;
 	
 	//Instatiation of register module
