8b66162edfc1d2ea302c51cc4093c4a03a4dbe73
anguslin <angus.a.lin@gmail.com>
2016-05-30 09:30:38 -0700

    Create counter.v

---
+++ counter.v
@@ -0,0 +1,14 @@
+//module for program counter
+
+module counter();
+        
+        `define countWidth 8;
+        input reset, loadpc;
+        output [7:0] out;
+        wire [7:0] loadValue, outToBeUpdated;
+        
+        assign loadValue = loadpc? out + 1'b1 : out;
+        assign outToBeUpdated= reset? 8'b00000000 : loadValue;
+        DFlipFlop #(`countWidth) loadOut(clk, outToBeUpdated, out);
+
+endmodule
