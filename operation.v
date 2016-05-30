d98c69b69a0f63edea4362dcfb55bfd19fd3d821
anguslin <angus.a.lin@gmail.com>
2016-05-30 12:06:34 -0700

    added overflow condition

--- operation.v
+++ operation.v
@@ -7,28 +7,28 @@ module operation (Ain, Bin, overflow, addSubVals, andVals, notBVal, sub, compute
         wire sign, lastNonSign;
         
         always@(*) begin
+        //subtraction or add
                 if(addSubVals) begin
                         assign overflow = sign ^ lastNonSign; //XOR gate; if not the same => overflow
-                        
                         //Arithmetic on Non Sign Bits
                         //if subtract, then convert to Two's Complement and then add
                         assign {lastNonSign, computedValue[width-2:0]}= {Ain[width-2:0] + Bin[width-2:0] ^ {width-1{sub}} + sub;
-                        
                         //Arithmetic on Sign Bits
                         assign {sign, computedValue[width-1]}= {Ain[width-1] + (Bin[width-1] ^ sub) + lastNonSign;
                 end
         end
         
         always@(*) begin
+        //if and A and B
                 if(andVals) begin
-                        computedValue = Ain & Bin;
+                        {computedValue, overflow} = {Ain & Bin, 1'b0};
                 end
         end
         
         always@(*) begin
+        //if not B 
                 if(notBVal) begin
-                        computedValue = ~Bin;
+                        {computedValue, overflow} = {~Bin, 1'b0};
                 end
         end
-
 endmodule
