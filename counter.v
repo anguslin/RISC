f2a16966f46b95911bb4bae64731463e60573fba
anguslin <angus.a.lin@gmail.com>
2016-05-30 09:32:17 -0700

    defined constant width

--- counter.v
+++ counter.v
@@ -2,13 +2,13 @@
 
 module counter(reset, loadpc, out);
         
-        `define countWidth 8;
+        `define COUNTWIDTH 8;
         input reset, loadpc;
         output [7:0] out;
         wire [7:0] loadValue, outToBeUpdated;
         
         assign loadValue = loadpc? out + 1'b1 : out;
         assign outToBeUpdated= reset? 8'b00000000 : loadValue;
-        DFlipFlop #(`countWidth) loadOut(clk, outToBeUpdated, out);
+        DFlipFlop #(`COUNTWIDTH) loadOut(clk, outToBeUpdated, out);
 
 endmodule
