ee9ce8c8c273747be67b95ededa0869f7a1d44ba
anguslin <angus.a.lin@gmail.com>
2016-05-30 10:02:41 -0700

    added sximm5

--- computation.v
+++ computation.v
@@ -1,10 +1,10 @@
-module computation(clk, asel, bsel, loadc, loads, shift, ALUop, datapath_in, A, B, status, C);
+module computation(clk, asel, bsel, loadc, loads, shift, ALUop, datapath_in, A, B, sximm5, status, C);
 
 	parameter width= 1;
 	parameter statusWidth= 1;
 	input clk, asel, bsel, loadc, loads;
 	input [1:0] shift, ALUop;
-	input [15:0] datapath_in, A, B;
+	input [15:0] datapath_in, A, B, sximm5;
 	output status;
 	output [15:0] C;
 	reg statusComputed;
@@ -14,7 +14,7 @@ module computation(clk, asel, bsel, loadc, loads, shift, ALUop, datapath_in, A, 
 	assign Ain= asel? {width{1'b0}}: A; 
 
 	//if bsel= 0 set Bin= to shifted B value else set Bin= to 11bits 0s + first 5 bits of datapath_in
-	assign Bin= bsel? {{11{1'b0}},datapath_in[4:0]}: BShift; 
+	assign Bin= bsel? sximm5: BShift; 
 
 	//Clock updates for status and C
 	DFlipFlopAllow #(statusWidth) loadStatusData(clk, loads, statusComputed, status); 	//status= running on a clock
