55f446b7d79e928f1e1aa8926a1197b8ada908ec
anguslin <angus.a.lin@gmail.com>
2016-05-30 12:51:57 -0700

    changed to assign statements to be synthesizable

--- operation.v
+++ operation.v
@@ -1,26 +1,26 @@
-module operation (Ain, Bin, overflow, addSubVals, andVals, notBVal, sub, computedValue);
+module operation (Ain, Bin, addSubVals, andVals, notBVal, sub, computedValue, overflow);
 
         parameter width= 1;
         input [width-1:0] Ain, Bin;
-        input overflow, addSubVals, andVals, notBVal, sub;
+        input addSubVals, andVals, notBVal, sub;
         output [width-1:0] computedValue;
+        output overflow;
         wire sign, lastNonSign;
         
-        always@(*) begin
-        //subtraction or add
-                case({addSubVals, andVals, notBVal})
-                        3'b100:
-                                overflow = sign ^ lastNonSign; //XOR gate; if not the same => overflow
-                                //Arithmetic on Non Sign Bits
-                                //if subtract, then convert to Two's Complement and then add
-                                {lastNonSign, computedValue[width-2:0]}= {Ain[width-2:0] + Bin[width-2:0] ^ {width-1{sub}} + sub;
-                                //Arithmetic on Sign Bits
-                                {sign, computedValue[width-1]}= {Ain[width-1] + (Bin[width-1] ^ sub) + lastNonSign;
-                        3'b010: //If and A with B
-                                {computedValue, overflow} = {Ain & Bin, 1'b0};
-                        3'b001: //If not B
-                                {computedValue, overflow} = {~Bin, 1'b0};
-                        default: 
-                                {computedValue, overflow} = {width+1{1'bx}};
-        end
+        //Adding or Subtracting Operation
+        assign overflow = addSubVals? sign ^ lastNonSign : 1'b0; //XOR gate; if not the same => overflow
+        //Arithmetic on Non Sign Bits
+        //if subtract, then convert to Two's Complement and then add
+        assign {lastNonSign, computedValue[width-2:0]}= addSubVals? {Ain[width-2:0] + Bin[width-2:0] ^ {width-1{sub}} + sub: {lastNonSign, computedValue[width-2:0]} ;
+        //Arithmetic on Sign Bits
+        assign {sign, computedValue[width-1]}= addSubVals? {Ain[width-1] + (Bin[width-1] ^ sub) + lastNonSign: {sign, computedValue[width-1]};
+       
+        //And Operation
+        //If and A with B
+        assign computedValue = andVals? Ain & Bin: computedValue;    
+        
+        //Not Operation
+        //If not B
+        assign computedValue = notBVal? ~Bin: computedValue;
+                      
 endmodule
