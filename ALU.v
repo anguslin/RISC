636c016dcaa95dbb8d095b76a340bf8c7f941c11
anguslin <angus.a.lin@gmail.com>
2016-05-30 10:46:38 -0700

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -1,6 +1,12 @@
 //ALU operations
 module ALU(ALUop, Ain, Bin, ALUComputedValue);
 
+	`define ADD 00
+	`define SUB 01
+	`define ANDVAL 10
+	`define NOTB 11
+	
+
 	parameter width= 1;
 	input [1:0] ALUop;
 	input [15:0] Ain, Bin;
@@ -9,10 +15,10 @@ module ALU(ALUop, Ain, Bin, ALUComputedValue);
 
 	always @(*) begin
 		case(ALUop)
-			2'b00: ALUComputedValue= Ain + Bin;		//if operation= 00 output= Ain + Bin
-			2'b01: ALUComputedValue= Ain - Bin;		//if operation= 01 output= Ain - Bin
-			2'b10: ALUComputedValue= Ain & Bin;		//if operation= 10 output= Ain AND Bin
-			2'b11: ALUComputedValue= ~Bin;			//if operation= 11 output= not Bin
+			`ADD: ALUComputedValue= Ain + Bin;		//if operation= 00 output= Ain + Bin
+			`SUB: ALUComputedValue= Ain - Bin;		//if operation= 01 output= Ain - Bin
+			`ANDVAL: ALUComputedValue= Ain & Bin;		//if operation= 10 output= Ain AND Bin
+			`NOTB: ALUComputedValue= ~Bin;			//if operation= 11 output= not Bin
 			default: ALUComputedValue= {width{1'bx}};	//default all x
 		endcase
 	end
