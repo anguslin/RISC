e6f7526847d110bc5efde0a4d73bd77e5cede242
anguslin <angus.a.lin@gmail.com>
2016-05-30 11:49:44 -0700

    separated status module

--- ALU.v
+++ ALU.v
@@ -1,25 +1,41 @@
 //ALU operations
-module ALU(ALUop, Ain, Bin, ALUComputedValue);
+module ALU(ALUop, Ain, Bin, ALUComputedValue, overflow);
 
 	`define ADD 00
 	`define SUB 01
 	`define ANDVAL 10
 	`define NOTB 11
-	
 
 	parameter width= 1;
+	input sub, overflow;
 	input [1:0] ALUop;
 	input [15:0] Ain, Bin;
 	output [15:0] ALUComputedValue;
+	output overflow;
 	reg [15:0] ALUComputedValue;
-
+	wire [15:0] computedValueTemp;
+	wire addSubVals, andVals, notBVal, sub;
+       
 	always @(*) begin
 		case(ALUop)
-			`ADD: ALUComputedValue= Ain + Bin;		//if operation= 00 output= Ain + Bin
-			`SUB: ALUComputedValue= Ain - Bin;		//if operation= 01 output= Ain - Bin
-			`ANDVAL: ALUComputedValue= Ain & Bin;		//if operation= 10 output= Ain AND Bin
-			`NOTB: ALUComputedValue= ~Bin;			//if operation= 11 output= not Bin
-			default: ALUComputedValue= {width{1'bx}};	//default all x
+			`ADD: {addSubVals, andVals, notBVal, sub}= {4'b1000}	
+			`SUB: {addSubVals, andVals, notBVal, sub}= {4'b1001}		
+			`ANDVAL: {addSubVals, andVals, notBVal, sub}= {4'b0100}		
+			`NOTB: {addSubVals, andVals, notBVal, sub}= {4'b0010}			
+			default: {addSubVals, andVals, notBVal, sub}= {4'bxxxx}	//default all x
 		endcase
 	end
+	
+	//Instatiate operation module to compute the specified operation
+	operation #(16) instatiateOperation(
+		.Ain(Ain), 
+		.Bin(Bin),  
+		.overflow(overflow), 
+		.addSubVals(addSubVals), 
+		.andVals(andVals), 
+		.notBVal(notBVal), 
+		.sub(sub), 
+		.computedValue(computedValue)
+		);
+
 endmodule
