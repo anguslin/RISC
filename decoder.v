5866196c439bc2d2f2f5b405b6cf2b6bfb3c6115
anguslin <angus.a.lin@gmail.com>
2016-05-30 17:46:50 -0700

    modified assign statements

--- decoder.v
+++ decoder.v
@@ -1,17 +1,17 @@
 //decoder module
-module decoder(instruction);
+module decoder(instruction, opcode, Rn, Rd, Rm, ALUop, op, shift, sximm5, sximm8);
         //inputs and outputs
-        input[15:0] instruction
+        input[15:0] instruction;
         output [2:0] opcode, Rn, Rd, Rm;
-        output [1:0] ALUop, op, shift
+        output [1:0] ALUop, op, shift;
         output [15:0] sximm5, sximm8;
         
         //assignments based off of instruction
         assign opcode = instruction[15:13];
         assign op = instruction[12:11];
         assign ALUop = instruction[12:11];
-        assign sximm5 = {10{instruction[4]},instruction[4:0]};
-        assign sximm8 = {7{instruction[7]},instruction[7:0]};
+        assign sximm5 = instruction[4]? {10'b1111111111,instruction[4:0]} : {10'b0000000000, instruction[4:0]};
+	assign sximm8 = instruction[7]? {7'b1111111,instruction[7:0]} : {7'b0000000, instruction[7:0]};
         assign shift = instruction[4:3];
         assign Rn = instruction[10:8];
         assign Rd = instruction[7:5];
