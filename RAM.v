dfa0a765782ce9717010607975c14ccefd4b7dc1
anguslin <angus.a.lin@gmail.com>
2016-05-31 14:04:04 -0700

    updated write

--- RAM.v
+++ RAM.v
@@ -1,10 +1,10 @@
-module RAM(clk, readAddress, writeAddress, write, in, out);
+module RAM(clk, readAddress, writeAddress, mwrite, in, out);
 
         parameter dataWidth = 16;       //size of data in each address
         parameter addrWidth = 8;        //size of address
         parameter filename = "data.txt";
 
-        input clk, write;
+        input clk, mwrite;
         input [addrWidth-1:0] readAddress, writeAddress;
         input [dataWidth-1:0] in;
         output [dataWidth-1:0] out;
@@ -14,7 +14,7 @@ module RAM(clk, readAddress, writeAddress, write, in, out);
         initial $readmemb(filename, mem);
         
         always @ (posedge clk) begin
-                if (write) begin
+                if (mwrite) begin
                         mem[writeAddress] <= in;
                 end
                 out <= mem[readAddress]; 
