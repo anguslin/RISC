991c5d5b20ac74f21ef0d406e243556c700c9dc5
anguslin <angus.a.lin@gmail.com>
2016-05-30 19:34:41 -0700

    modified name of address

--- RAM.v
+++ RAM.v
@@ -1,15 +1,15 @@
 module RAM(clk, readAddress, writeAddress, write, in, out);
 
-        parameter dataWidth = 32; //size of data
-        parameter multWidth = 4;	//4 bytes needed per 32 bits of data 
+        parameter dataWidth = 32;       //size of data in each address
+        parameter addrWidth = 4;        //size of address
         parameter filename = "data.txt";
 
         input clk, write;
-        input [multWidth-1:0] readAddress, writeAddress;
+        input [addrWidth-1:0] readAddress, writeAddress;
         input [dataWidth-1:0] in;
         output [dataWidth-1:0] out;
         reg [dataWidth-1:0] out;
-        reg [dataWidth-1:0] mem[2**multWidth-1:0];
+        reg [dataWidth-1:0] mem[2**addrWidth-1:0];
         
         initial $readmemb(filename, mem);
         
