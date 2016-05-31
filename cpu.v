01cfb6926f51534a28bfdb8d3fbd627eb0eb6274
anguslin <angus.a.lin@gmail.com>
2016-05-31 11:15:42 -0700

    still need to fix first state

--- cpu.v
+++ cpu.v
@@ -1,6 +1,4 @@
 module decoder(instruction, nsel, opcode, readnum, writenum, ALUop, op, shift, sximm5, sximm8);
-        //inputs and outputs
-        input[15:0] instruction;
         input [2:0] nsel;
         output [2:0] opcode, readnum, writenum;
         output [1:0] ALUop, op, shift;
@@ -42,3 +40,51 @@ module instructionReg(clk, mdata, loadir, instruction);
 //after all the things are done in each state
 //You should load the instruction register by enable loadir = 1 or something along the lines
 //do this for all the operations
+
+00: tempReg = Rn; //nsel 00 = Rn
+	        	01: tempReg = Rd; //nsel 01 = Rd
+	        	10: tempReg = Rm; //nsel 10 = Rm
+
+//START HERE
+
+module cpu();
+//STATES
+//instr 1 MOV Rn,#<imm8> take bits 0 to 7 of instruction (imm8) and put it in the register identified by register Rn bits 8-10 -> nsel=00 
+`define MOV 3'b110
+`define INSTRTOREG 2'b10
+//instr 2 Reads Rm (nsel = 10) into datapath reg B, sets asel = 1 (to get all 0s), in the ALU it adds together to put in register C -> then written to Rd
+`define REGTOREG 2'b00
+
+//REGISTER choosing
+`define RN 2'b00
+`define RD 2'b01
+`define RM 2'b10
+
+//VSEL to choose with data to let in for the writing part
+`define MDATA 2'b00
+`define SXIMM8 2'b01
+`define ZEROANDPC 2'b10
+`define C 1'b1
+
+//TRUE OR FALSE
+`define ON 1'b1
+`define OFF 1'b0
+
+
+wire [4:0] currentState, nextState; 
+
+always @(*)
+	case(currentstate)
+	{MOV,INSTRTOREG}: {nsel,vsel,write ={`RN,`SXIMM8VSEL,`ON, 
+
+	case(vsel)
+			00: data_in= mdata; 
+			01: data_in= sximm8;
+			10: data_in= {8'b00000000, PC};
+			11: data_in= datapath_out;
+
+
+
+ output [2:0] opcode, readnum, writenum;
+        output [1:0] ALUop, op, shift;
+        output [15:0] sximm5, sximm8;
