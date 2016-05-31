4eeeced61d33ccd81513fa7cd65da00779382e61
anguslin <angus.a.lin@gmail.com>
2016-05-30 18:33:53 -0700

    added module instantiation

--- topMod.v
+++ topMod.v
@@ -1,67 +1,64 @@
 // DE1-SOC Interface Specification
 
 // KEY0= rising lock edge (When pressed) 
-// LEDR9= status register output
+// LEDR[9:7]= status register output
 // HEX3, HEX2, HEX1, HEX0= wired to datapath_out for LED display
-
-// control signal(s)  switch(es)
-// ~~~~~~~~~~~~~~~~~  ~~~~~~~~~       
-// Register Stage
-//           readnum  SW[3:1]
-//             loada  SW[5]
-//             loadb  SW[6]
-// Computation Stage
-//             shift  SW[2:1]
-//              asel  SW[3]
-//              bsel  SW[4]
-//             ALUop  SW[6:5]
-//             loadc  SW[7]
-//             loads  SW[8]
-// Write Stage
-//             write  SW[0]      
-//          writenum  SW[3:1]
-//              vsel  SW[4]
 
 module top(KEY, SW, CLOCK_50, LEDR, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5);
 
         input [3:0] KEY;
         input [9:0] SW;
-        input CLOCK_50;
+        input CLOCK_50; //VERIFY THE PURPOSE OF THIS
         output [9:0] LEDR; 
         output [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
-        wire [15:0] datapath_out, datapath_in;
+        wire [15:0] datapath_out, mdata;
         wire write, vsel, loada, loadb, asel, bsel, loadc, loads;
-        wire [2:0] readnum, writenum;
+        wire [2:0] readnum, writenum, nsel;
         wire [1:0] shift, ALUop;
-        
           
-        datapath datapathInstatiate( 
-                        .clk(~KEY[0]),
-          
-                        //register module
-                        .readnum(readnum),
-                        .vsel(vsel),
-                        .loada(loada),
-                        .loadb(loadb),
-                        
-                        //computation + alu + shift modules
-                        .shift(shift),
-                        .asel(asel),
-                        .bsel(bsel),
-                        .ALUop(ALUop),
-                        .loadc(loadc),
-                        .loads(loads),
-                        
-                        // write module
-                        .writenum(writenum),
-                        .write(write),  
-                        .datapath_in(datapath_in),
-        
-                        // outputs
-                        .status(LEDR[9]),
-                        .datapath_out(datapath_out)
-                     );
-        
+        datapath datapathInstantiate( 
+                .clk(~KEY[0]),
+                //register module
+                .readnum(readnum),
+                .vsel(vsel),
+                .loada(loada),
+                .loadb(loadb),
+                //computation + alu + shift modules
+                .shift(shift),
+                .asel(asel),
+                .bsel(bsel),
+                .ALUop(ALUop),
+                .loadc(loadc),
+                .loads(loads),
+                // write module
+                .writenum(writenum),
+                .write(write),  
+                .mdata(mdata),
+                // outputs
+                .status(LEDR[9:7]),
+                .datapath_out(datapath_out)
+                );
+                     
+                     
+        decoder decoderInstantiate(
+                .instruction(mdata),
+                .nsel(nsel),
+                .opcode(opcode), 
+                .readnum(Rn), 
+                .writenum(writenum), 
+                .ALUop(ALUop), 
+                .op(op), 
+                .shift(shift), 
+                .sximm(sximm5), 
+                .sximm8(sximm8)
+                );
+                     
+        instructionReg instructionRegInstantiate(
+                .clk(~KEY[0]), 
+                .mdata(mdata), 
+                .loadir(loadir), 
+                .instruction(instruction)
+                );
         
         // assigned HEX display to datapath_out values
         HEXDisplay HEX0Instantiate(datapath_out[3:0], HEX0);   
