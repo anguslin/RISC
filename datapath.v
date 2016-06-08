f8e74b47fb3173a387f3376c2ed48b19fabd726c
anguslin <angus.a.lin@gmail.com>
2016-06-07 18:49:43 -0700

    updated ports

--- datapath.v
+++ datapath.v
@@ -1,80 +1,52 @@
-//Datapath module of the RISC
-module datapath(clk, readnum, vsel, loada, loadb, shift, asel, bsel, ALUop, loadc, loads, writenum, write, mdata, sximm5, sximm8, status, B, C);
-
-	//constants to define
-	`define WIDTH 16
-	`define STATUSWIDTH 3
-
-	input clk, loada, loadb, write, asel, bsel, loadc, loads;
-	input [2:0] readnum, writenum;
-	input [1:0] shift, ALUop, vsel;
-	input [15:0] mdata, sximm5, sximm8;
-	output [2:0] status;
-	output [15:0] B, C;
-
-	wire [15:0] A, reg0, reg1, reg2, reg3, reg4, reg5, reg6, reg7;
-
-	wire [7:0] PC; //no function currently
-	
-	//Instantiation of register module
-	register #(
-		.width(`WIDTH)
-		) instantiateReg(
-	 	.clk(clk),
-		.loada(loada),
-		.loadb(loadb),
-		.write(write),
-		.readnum(readnum),
-		.reg0(reg0),
-		.reg1(reg1),
-		.reg2(reg2),
-		.reg3(reg3),
-		.reg4(reg4),
-		.reg5(reg5),
-		.reg6(reg6),
-		.reg7(reg7),
-		.A(A),
-		.B(B)
-	 );
-	 
-	//Instatiation of computation module
-	computation #(
-		.width(`WIDTH),
-		.statusWidth(`STATUSWIDTH) 
-		) instantiateComp(
-		.clk(clk), 
-		.asel(asel), 
-		.bsel(bsel), 
-		.loadc(loadc), 
-		.loads(loads), 
-		.shift(shift), 
-		.ALUop(ALUop),
-		.A(A), 
-		.B(B), 
-		.sximm5(sximm5),
-		.status(status), 
-		.C(C)
-	);
-
-	//Instantiation of write module
-	write #(
-		.width(`WIDTH)
-		) instantiateWrite(
-		.clk(clk), 
-		.vsel(vsel), 
-		.write(write),
-		.writenum(writenum), 
-		.C(C), 
-		.mdata(mdata), 
-		.sximm8(sximm8),
-		.PC(PC),
-		.reg0(reg0), 
-		.reg1(reg1), 
-		.reg2(reg2), 
-		.reg3(reg3), 
-		.reg4(reg4), 
-		.reg5(reg5), 
-		.reg6(reg6), 
-		.reg7(reg7)
-	);
+module datapath (clk, hi, lo, fmapAddrSel, jSel, iSel, eclassSel, fmapDataSel, loadtmp, loadec_tmp, loadi, loadj, notReturn, cont1, cont2, cont3);
+        input [31:0] hi, lo;           		     //input of function
+        input [3:0] fmapAddrSel, jSel; 		     //4 bit mux selecter;
+        input [2:0] iSel;              		     //3 bit mux selecter;
+        input [1:0] eclassSel, fmapDataSel;          //2 bit mux selecter;
+        input clk, loadi, loadj, loadtmp, loadec_tmp;
+        output notReturn, cont1, cont2, cont3;
+        wire [31:0] i, j, tmp, ec_tmp, fmapOut, eclassOut;
+      
+        memory memoryInstant(
+                .i(i), 
+                .j(j), 
+                .tmp(tmp), 
+                .fmapAddrSel(fmapAddrSel), 
+                .eclassSel(eclassSel), 
+                .fmapDataSel(fmapDataSel), 
+                .eclassMemWrite(eclassMemWrite), 
+                .fmapMemWrite(fmapMemWrite), 
+                .clk(clk), 
+                .fmapOut(fmapOut), 
+                .eclassOut(eclassOut)
+                );
+                
+        register registerInstant(
+                .hi(hi), 
+                .lo(lo), 
+                .iSel(iSel), 
+                .eclassSel(eclassSel), 
+                .clk(clk), 
+                .iLoad(iLoad), 
+                .jLoad(jLoad), 
+                .tmpLoad(tmpLoad), 
+                .ec_tempLoad(ec_tmpLoad), 
+                .i(i), 
+                .j(j), 
+                .tmp(tmp),
+                .ec_tmp(ec_tmp)
+                );
+                
+        verify verifyInstant(
+                .hi(hi), 
+                .lo(lo),
+                .i(i), 
+                .j(j), 
+                .eclassOut(eclassOut), 
+                .ec_tmp(ec_tmp), 
+                .notReturn(notReturn), 
+                .cont1(cont1), 
+                .cont2(cont2), 
+                .cont3(cont3)
+                );
 endmodule
