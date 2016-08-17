f89e6331f262c3c3f0c1fd1d70e508b8190e7583
anguslin <anguslin@users.noreply.github.com>
2016-08-16 19:09:28 -0700

    Update datapath.v

--- datapath.v
+++ datapath.v
@@ -72,8 +72,8 @@ register #(
 		.reg1(reg1), 
 		.reg2(reg2), 
 		.reg3(reg3), 
--		.reg4(reg4), 
--		.reg5(reg5), 
--		.reg6(reg6), 
--		.reg7(reg7)
--	);
+		.reg4(reg4), 
+		.reg5(reg5), 
+		.reg6(reg6), 
+		.reg7(reg7)
+	);
